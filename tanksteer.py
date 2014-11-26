c94c76706adf80cb99def02c86098db672a5d8b0
Brian Starkey <stark3y@gmail.com>
2014-11-26 18:01:43 +0000

    tanksteer: Add localiser to Tanksteer
    This adds the Localiser class to Tanksteer, along with other refinements
    to the tanksteer in general

--- tanksteer.py
+++ tanksteer.py
@@ -1,3 +1,4 @@
+import logging
 import math
 import threading
 import smbus
@@ -5,39 +6,69 @@ import time
 
 import wheel
 
-bus = smbus.SMBus(1)
-
-settings = {
-    'tau': 0.05,
-    'chassis_width': 159.5,
-    'wheel_diameter': 70.0,
-    'speed_limiter': 0.8,
-    'wheel_settings': {
-        'left': {
-            'i2c_bus': bus,
-            'addr': 0x41,
-            'servo': 0,
-            'threshold': 0x60,
-            'slots': 60,
-            'curves': [((1413, 1470), (-100,  -1)),\
-                       ((1471, 1479), (  0,  0)),\
-                       ((1480, 1536), (  1, 100))],
-        },
-        'right': {
-            'i2c_bus': bus,
-            'addr': 0x40,
-            'servo': 1,
-            'threshold': 0x60,
-            'slots': 60,
-            'curves': [((1540, 1480), (-100, -1)),
-                       ((1479, 1471), (0, 0)),
-                       ((1471, 1407), (1, 100))],
-        },
-    },
-}
-
 class Tanksteer:
+    class Localiser:
+        def __init__(self, b, odometer):
+            self.logger = logging.getLogger(self.__class__.__name__)
+            self.logger.info("Localiser init")
+            self.b = b
+            self.reset(odometer)
+            self.last_time = time.time()
+
+        def reset(self, odometer):
+            self.last = odometer
+            # TODO: What direction is "forwards"?
+            # This assumes that forwards is along the positive Y
+            self.theta = math.pi / 2
+            self.pos = (0, 0)
+
+        def clamp_theta(self, theta):
+            if (theta > math.pi):
+                return theta - (2 * math.pi)
+            elif (theta < -math.pi):
+                return theta + (2 * math.pi)
+            else:
+                return theta
+
+        def update(self, odometer):
+            #now = time.time()
+            #if (now - self.last_time < 0.2):
+            #    return
+            #self.last_time = now
+            distance_moved = (odometer[0] - self.last[0], \
+                    odometer[1] - self.last[1])
+            s = ''
+
+            # TODO: Improve the fuzzyness here
+            if (distance_moved[0] == distance_moved[1]):
+                    #(0.05 * abs(distance_moved[0] + distance_moved[1]))):
+                delta_theta = 0.0
+                delta_s = (distance_moved[0] * math.cos(self.theta), \
+                           distance_moved[1] * math.sin(self.theta))
+                s = 'line'
+            else:
+                turn_r = (self.b / 2) * \
+                         ((distance_moved[1] + distance_moved[0]) / \
+                          (distance_moved[1] - distance_moved[0]))
+                delta_theta = (distance_moved[1] - distance_moved[0]) / self.b
+                delta_x = turn_r * \
+                        (math.sin(delta_theta + self.theta) - \
+                         math.sin(self.theta))
+                delta_y = -turn_r * \
+                        (math.cos(delta_theta + self.theta) - \
+                         math.cos(self.theta))
+                delta_s = (delta_x, delta_y)
+                s = 'arc'
+            self.last = odometer
+            self.pos = (self.pos[0] + delta_s[0], self.pos[1] + delta_s[1])
+            self.theta = self.clamp_theta(self.theta + delta_theta)
+            self.logger.debug("Update: moved(%s):%f %f pos:%f %f",
+                    s, distance_moved[0], distance_moved[1], \
+                            self.pos[0], self.pos[1])
+
     def __init__(self, settings):
+        self.logger = logging.getLogger(self.__class__.__name__)
+        self.logger.info("Tanksteer init")
         self.b = settings['chassis_width']
         self.tau = settings['tau']
         self.slots_per_mm = settings['wheel_settings']['left']['slots'] / \
@@ -50,14 +81,16 @@ class Tanksteer:
         self.target = (0, 0)
         self.cur_speed = (0, 0)
         self.update((0, 0), (0, 0))
+        self.localiser = Tanksteer.Localiser(self.b, self.odometer)
+        self.auto = False
 
         # Set up speeds
         self.max_speed = settings['speed_limiter'] * \
                 min([w.max_speed() for w in self.wheels])
-        print "Max speed: {}".format(self.max_speed)
+        self.logger.info("Max speed: {}".format(self.max_speed))
         # Some conservative default
         #self.default_speed = 0.5 * self.max_speed
-        self.default_speed = 20
+        self.default_speed = 50
 
         # Spawn a thread to tick the wheels
         self.tick_thread = threading.Thread(target=self.__loop)
@@ -71,12 +104,20 @@ class Tanksteer:
             if (time_now >= next_time):
                 next_time = time_now + self.tau
                 self.tick()
-                for i in range(len(self.wheels)):
-                    d = self.odometer[i] - self.target[i]
-                    if ((self.cur_speed[i] > 0) and (d >= 0)) or \
-                            ((self.cur_speed[i] < 0) and (d <= 0)):
-                        self.stop()
-                        break
+                self.localiser.update(self.odometer)
+                if self.auto:
+                    for i in range(len(self.wheels)):
+                        d = self.odometer[i] - self.target[i]
+                        if ((self.cur_speed[i] > 0) and (d >= 0)) or \
+                                ((self.cur_speed[i] < 0) and (d <= 0)):
+                            self.stop()
+                            break
+
+    def heading(self):
+        return self.localiser.theta
+
+    def position(self):
+        return self.localiser.pos
 
     def stop(self):
         self.update((0, 0), (0, 0))
@@ -86,34 +127,42 @@ class Tanksteer:
         self.wheels[1].tick()
         if (self.cur_speed[0] != 0) or (self.cur_speed[1] != 0):
             actual = (self.wheels[0].speed, self.wheels[1].speed)
-            error = (self.cur_speed[0] - actual[0], self.cur_speed[1] - actual[1])
-            print "set: (%2.2f, %2.2f) actual: (%2.2f, %2.2f) error: (%2.2f, %2.2f)" % (
-                    self.cur_speed[0], self.cur_speed[1],
-                    actual[0], actual[1], error[0], error[1])
-        self.odometer = (self.wheels[0].count, self.wheels[1].count)
+            error = (self.cur_speed[0] - actual[0], \
+                     self.cur_speed[1] - actual[1])
+            self.logger.debug("set: (%2.2f, %2.2f) " \
+                    "actual: (%2.2f, %2.2f) error: (%2.2f, %2.2f)" % \
+                    (self.cur_speed[0], self.cur_speed[1], \
+                    actual[0], actual[1], error[0], error[1]))
+        self.odometer = (self.slots_to_mm(self.wheels[0].count), \
+                self.slots_to_mm(self.wheels[1].count))
 
-    def update(self, speed, distance):
+    def update(self, speed, distance = (0, 0), auto = False):
         self.cur_speed = speed
         self.wheels[0].set_speed(self.cur_speed[0])
         self.wheels[1].set_speed(self.cur_speed[1])
-        self.tick()
         self.target = (self.odometer[0] + distance[0],
                        self.odometer[1] + distance[1])
-        print "Odo: {}".format(self.odometer)
-        print "Distance: {}".format(distance)
-        print "Target: {}".format(self.target)
+        self.auto = auto
+        self.tick()
+        self.logger.debug( "Odo: {}".format(self.odometer))
+        self.logger.debug( "Distance: {}".format(distance))
+        self.logger.debug( "Target: {}".format(self.target))
 
     def mm_to_slots(self, mm):
         return mm * self.slots_per_mm
 
     def slots_to_mm(self, slots):
-        return (1 / self.slots_mm) * slots
+        return (1 / self.slots_per_mm) * slots
 
     def speed(self, speed):
+        print "Input speed: {}".format(speed)
         if speed == None:
             return self.default_speed
         speed = max(min(1.0, speed), 0.0)
-        return self.max_speed * speed
+        print "Clamped speed: {}".format(speed)
+        speed = self.max_speed * speed
+        print "Output speed: {}".format(speed)
+        return speed
 
     def turn_deg(self, radius, angle, speed = None):
         self.turn_rad(radius, math.radians(angle), speed = None)
@@ -128,12 +177,13 @@ class Tanksteer:
     #   -ve     +ve     Backwards, turning left
     def turn_rad(self, radius, angle, speed = None):
         delta_r = self.b / 2.0
-        l_distance = self.mm_to_slots((radius - delta_r) * angle)
-        r_distance = self.mm_to_slots((radius + delta_r) * angle)
+        l_distance = ((radius - delta_r) * angle)
+        r_distance = ((radius + delta_r) * angle)
 
         # v_l = (2r - b)/(2r + b)*v_r
         # v_r = (2r + b)/(2r - b)*v_l
         velocity = math.copysign(self.speed(speed), radius / angle)
+        velocity = velocity / 2
         d = 2.0 * radius
         if radius >= 0:
             # Right wheel is fastest.
@@ -141,17 +191,20 @@ class Tanksteer:
             v_r = velocity
             v_l = (d - self.b) / (d + self.b) * v_r
         else:
+            # Left wheel is fastest.
+            # Fix left wheel. Work out right
             v_l = velocity
             v_r = (d + self.b) / (d - self.b) * v_l
 
-        self.update((v_l, v_r), (l_distance, r_distance))
+        self.update((v_l, v_r), (l_distance, r_distance), True)
 
     # Speed should be 0 - 1
     # Distance (in mm) sets direction
     def line(self, distance, speed = None):
-        v_r = math.copysign(self.speed(speed), distance)
-        v_l = v_r
-        distance_slots = self.mm_to_slots(distance)
-        self.update((v_l, v_r), (distance_slots, distance_slots))
+        if abs(distance) > 1:
+            v_r = math.copysign(self.speed(speed), distance)
+            v_l = v_r
+            #distance_slots = self.mm_to_slots(distance)
+            self.update((v_l, v_r), (distance, distance), True)
 
 
