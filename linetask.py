06e59b5c5d2b1f18f9ae48b9d856b02388b6dca1
Brian Starkey <stark3y@gmail.com>
2014-12-03 21:39:09 +0000

    linetask: Improve logic for line following
    Better line following ability. The "lost" routine is a bit naff, but
    better than nothing

--- linetask.py
+++ linetask.py
@@ -1,3 +1,4 @@
+import logging
 import math
 import random
 
@@ -5,12 +6,15 @@ class LineFollowerTask:
     STATE_FOLLOWING = 0
     STATE_LOST = 1
     def __init__(self):
+        self.logger = logging.getLogger(self.__class__.__name__)
         self.last_known = None
         self.default_speed = 0.60
         self.last_seen = 0.0
         self.state = self.STATE_FOLLOWING
         self.lost_timer = 0
-        self.lost_timeout = 10
+        self.lost_timeout = 8
+        self.lost_step = 0
+        self.lost_turn = math.pi / 2
 
     def plan(self, readings):
         if not readings.has_key('LineSensor'):
@@ -20,9 +24,12 @@ class LineFollowerTask:
             if reading == None:
                 if self.state == self.STATE_FOLLOWING:
                     self.lost_timer = self.lost_timer + 1
+                    # We're properly lost
                     if (self.lost_timer >= self.lost_timeout):
                         self.state = self.STATE_LOST
-                    # Panic a little bit
+                        self.lost_step = 0
+
+                    # Only panic a little bit
                     if self.last_seen == 0:
                         # We were going straight, pick a random direction
                         sign = random.choice([-1, 1])
@@ -31,13 +38,80 @@ class LineFollowerTask:
                         reading = math.copysign(abs(self.last_seen * 2), \
                                 self.last_seen)
                 else:
-                    self.state = self.STATE_LOST
                     # Now we need to go into full-on search mode
-                    return { 'd_theta': math.copysign(math.pi / 6, \
-                            self.last_seen) }
+                    if readings['auto']:
+                        # Wait until we finished the last bit
+                        return {}
+
+                    actions = {}
+                    if self.lost_step == 0:
+                        # Turn
+                        self.logger.debug(\
+                                "Lost stage {}!".format(self.lost_step))
+                        actions = { 'd_theta': \
+                                math.copysign(self.lost_turn, \
+                                self.last_seen) }
+                    elif self.lost_step == 1:
+                        # Line
+                        self.logger.debug(\
+                                "Lost stage {}!".format(self.lost_step))
+                        actions = { 'distance': 200 }
+                    elif self.lost_step == 2:
+                        # Turn
+                        self.logger.debug(\
+                                "Lost stage {}!".format(self.lost_step))
+                        actions = { 'd_theta': \
+                                math.copysign(self.lost_turn, \
+                                self.last_seen) }
+                    elif self.lost_step == 3:
+                        # Line
+                        self.logger.debug(\
+                                "Lost stage {}!".format(self.lost_step))
+                        actions = { 'distance': 250 }
+                    elif self.lost_step == 4:
+                        # Turn back
+                        self.logger.debug(\
+                                "Lost stage {}!".format(self.lost_step))
+                        actions = { 'd_theta': \
+                                -math.copysign(self.lost_turn, \
+                                self.last_seen) }
+                    elif self.lost_step == 5:
+                        # Long line
+                        self.logger.debug(\
+                                "Lost stage {}!".format(self.lost_step))
+                        actions = { 'distance': 250 }
+                    elif self.lost_step == 6:
+                        # Turn back
+                        self.logger.debug(\
+                                "Lost stage {}!".format(self.lost_step))
+                        actions = { 'd_theta': \
+                                -math.copysign(self.lost_turn, \
+                                self.last_seen) }
+                    elif self.lost_step == 7:
+                        # Long line
+                        self.logger.debug(\
+                                "Lost stage {}!".format(self.lost_step))
+                        actions = { 'distance': 250 }
+                    elif self.lost_step == 8:
+                        # Arc one way
+                        self.logger.debug(\
+                            "Lost stage {}!".format(self.lost_step))
+                        actions = { 'arc': { 'radius': 300, \
+                                'angle': 2 * math.pi } }
+                    elif self.lost_step == 9:
+                        # Arc the other
+                        self.logger.debug(\
+                                "Lost stage {}!".format(self.lost_step))
+                        actions = { 'arc': { 'radius': 300, \
+                                'angle': -2 * math.pi } }
+                    else:
+                        raise RuntimeError("I got lost :-(")
+                    self.lost_step = self.lost_step + 1
+                    return actions
             else:
                 self.last_seen = reading
                 self.state = self.STATE_FOLLOWING
+                self.lost_timer = 0
             diff = self.default_speed * abs(reading) * 0.8
             if reading <= 0.0:
                 left = self.default_speed
